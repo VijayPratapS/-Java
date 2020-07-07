c69cbd196feb6855ccf44e2cc7a4feffc2c17041
vijayp780540 <67731905+vijayp780540@users.noreply.github.com>
2020-07-07 16:54:02 +0530

    Supplier with stream api

---
+++ Suplier.java
@@ -0,0 +1,23 @@
+package java8prj;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.base.Supplier;
+
+public class Suplier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello to java world";
+				Suplier.suplrTst(str);
+	}
+	static void suplrTst(String s)
+	{
+		Supplier<String> suplr=()->s;
+		List<String> lst=Arrays.asList("a","b");
+		String s1=lst.stream().findAny().orElseGet(suplr);
+		System.out.println(s1);
+	}
+
+}
